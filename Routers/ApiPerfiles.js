904b31314cf4463f6baf049b9e6375d224ef92fc
ElvisAlas <elvisalasecheverry@gmail.com>
2024-05-13 14:43:16 -0600

    correccion del url perfilesController

--- Routers/ApiPerfiles.js
+++ Routers/ApiPerfiles.js
@@ -2,7 +2,7 @@ import Express, { request } from "express";
 
 const Perfiles = Express();
 
-import {getPerfil,postPerfil} from "../Controllers/PerfilesController.js";
+import {getPerfil,postPerfil} from "../controllers/PerfilesController.js";
 
 Perfiles.get('',getPerfil)
 Perfiles.post('',postPerfil)
