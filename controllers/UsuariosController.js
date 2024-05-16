607b110767c8acfb9195173752295ece48030542
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2024-05-15 18:47:44 -0600

    Update UsuariosController.js

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -6,7 +6,7 @@ const postNuevoUsuario = async (req, res) => {
         const { buffer } = req.file;
         const params = [nombre_usuario, nombre, apellido, correo, contrasenia, fechaNacimiento, buffer];
         
-        // Asegurarse de que el SQL sea consistente y agregar el alias 'mensaje' correctamente
+        // A
         const sql = `INSERT INTO usuarios (nombre_usuario, nombre, apellido, correo, contrasenia, fechaNacimiento, foto)
                      VALUES ($1, $2, $3, $4, $5, $6, $7)
                      RETURNING *, 'Insercion Exitosa' AS mensaje`;
