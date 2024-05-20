6ebd8572c61b7920c3c22c4b23ad102db35c1738
Elvis Alas <110005376+ElvisAlas@users.noreply.github.com>
2024-05-19 19:55:39 -0600

    resubiendo los cambios de usuarios

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -1,7 +1,7 @@
 import { db } from "../db/conn.js";
 
 const getUsuarios = async (req, res) => {
-    const sql = `SELECT * FROM usuarios;`;
+    const sql = `SELECT *,encode(fotoPerfil, 'base64') AS foto FROM usuarios;`;
     const result = await db.query(sql);
   
     res.json(result);
@@ -11,25 +11,25 @@ const getUsuariosByNombreUsuario = async (req, res) => {
   const params = [req.params.nombre_usuario];
   const sql = `SELECT a.id, a.nombre_usuario, a.nombre || ' ' || a.apellido "nombre", a.correo, a.idPerfil, b.nombre "perfil" FROM usuarios a inner join perfil b on a.idPerfil = b.id where a.nombre_usuario = $1;`;
   const result = await db.query(sql, params);
-
+console.log(result)
   res.json(result);
 }
 
 const postNuevoUsuarios = async (req, res) => {
     try {
-        const { nombre_usuario, nombre, apellido, correo, contrasenia, fechaNacimiento, idPerfil } = req.body;
+        const { nombre_usuario, nombre, apellido, correo, contrasenia, fechanacimiento, idPerfil } = req.body;
         const buffer = req.file ? req.file.buffer : null;
 
-        const params = [nombre_usuario, nombre, apellido, correo, contrasenia, fechaNacimiento, buffer, idPerfil];
+        const params = [nombre_usuario, nombre, apellido, correo, contrasenia, fechanacimiento, buffer, idPerfil];
 
         const sql = `
-            INSERT INTO usuarios (nombre_usuario, nombre, apellido, correo, contrasenia, fechaNacimiento, fotoPerfil, idPerfil)
+            INSERT INTO usuarios (nombre_usuario, nombre, apellido, correo, contrasenia, fechanacimiento, fotoPerfil, idPerfil)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING *, 'Insercion Exitosa' AS mensaje;
         `;
 
         const result = await db.query(sql, params);
-        res.json(result.rows[0]);
+        res.json(result);
     } catch (err) {
         console.error('Error al insertar un nuevo usuario:', err);
         res.status(500).json({ mensaje: err.message });
@@ -43,7 +43,7 @@ const putUsuarios = async (req, res) => {
 
     const sql = `
         UPDATE usuarios
-        SET nombre_usuario = $1, nombre = $2, apellido = $3, correo = $4, contrasenia = $5, fechaNacimiento = $6, fotoPerfil = $7, idPerfil = $8
+        SET nombre_usuario = $1, nombre = $2, apellido = $3, correo = $4, contrasenia = $5, fechanacimiento = $6, fotoPerfil = $7, idPerfil = $8
         WHERE id = $9
         RETURNING *;
     `;
@@ -64,25 +64,10 @@ const putUsuarios = async (req, res) => {
 }
 
 const deleteUsuarios = async (req, res) => {
-    const { id } = req.params;
-
-    const sql = `
-        DELETE FROM usuarios
-        WHERE id = $1
-        RETURNING *;
-    `;
-
-    try {
-        const result = await db.query(sql, [id]);
-        if (result.rows.length === 0) {
-            res.status(404).json({ message: 'Usuario no encontrado' });
-        } else {
-            res.json({ message: 'Usuario eliminado exitosamente' });
-        }
-    } catch (error) {
-        console.error('Error al eliminar el usuario:', error);
-        res.status(500).json({ message: 'Error al eliminar el usuario' });
-    }
+    const params = [req.params.id];
+    const sql = `delete from  usuarios where id=$1 returning id,'Eliminado' mensaje  `
+    const result = await db.query(sql, params);
+    res.json(result);
 }
 
 export { getUsuarios, getUsuariosByNombreUsuario, postNuevoUsuarios, putUsuarios, deleteUsuarios };
