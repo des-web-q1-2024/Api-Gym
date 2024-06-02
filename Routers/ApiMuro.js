1849a342839368f3fce8c4eba1a90489bce88ff6
Elvis Alas <110005376+ElvisAlas@users.noreply.github.com>
2024-06-02 17:07:16 -0600

    adicionando comentarios e hilos

--- Routers/ApiMuro.js
+++ Routers/ApiMuro.js
@@ -13,11 +13,26 @@ import {
   getSaveEvento,
   postInsertPost,
   getPost,
+  getLikeCountPost,
+  postLikeEvento,
+  deleteLikeEvento,
+  postInsertHiloPost,
+  getHiloPost,
 } from "../controllers/MuroController.js";
 
+// like evento
+Muro.get("/:idPost", getLikeCount);
 Muro.post("", postLike);
 Muro.delete("", deleteLike);
-Muro.get("/:idPost", getLikeCount);
+
+
+// like post
+Muro.get("/post/comentario/:idPost", getLikeCountPost);
+Muro.get("/post/Hilocomentario/:idPost", getHiloPost);
+Muro.post("/post/comentario", postLikeEvento);
+Muro.delete("/post/comentario", deleteLikeEvento);
+
+
 
 // evento
 Muro.get("/saveEvent/:idUsuarios", getSaveEvento);
@@ -26,6 +41,6 @@ Muro.delete("/saveEvent", deleteSaveEvento);
 
 Muro.get("/post/:idUsuarios",getPost);
 Muro.post("/post", upload.single('foto'), postInsertPost);
-
+Muro.post("/post/hilo", upload.single('foto'), postInsertHiloPost);
 
 export { Muro };
--- controllers/MuroController.js
+++ controllers/MuroController.js
@@ -1,9 +1,11 @@
 import { db } from "../db/conn.js";
 
+//like eventos
+
 const getLikeCount = async (req, res) => {
   const params = [req.params.idPost];
   const sql = `select count(1)  as like
-                from EstadoLike where idpost=$1
+                from EstadoLike where idevento=$1
                     `;
   const result = await db.query(sql, params);
 
@@ -12,11 +14,11 @@ const getLikeCount = async (req, res) => {
 
 const postLike = async (req, res) => {
   try {
-    const { idPost, idUsuarios } = req.body;
-    const params = [idPost, idUsuarios];
+    const { idEvento, idUsuarios } = req.body;
+    const params = [idEvento, idUsuarios];
 
     const sql = `
-      INSERT INTO estadolike (idPost, idUsuarios)
+      INSERT INTO estadolike (idEvento, idUsuarios)
       VALUES ($1, $2)
       RETURNING *;
     `;
@@ -34,7 +36,56 @@ const deleteLike = async (req, res) => {
     const params = [idPost, idUsuarios];
     const sql = `
         DELETE FROM estadolike
-        WHERE idPost = $1 AND idUsuarios = $2
+        WHERE idEvento = $1 AND idUsuarios = $2
+        RETURNING *;
+      `;
+    const result = await db.query(sql, params);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al eliminar like" });
+  }
+};
+
+
+
+// like post
+
+const getLikeCountPost = async (req, res) => {
+  const params = [req.params.idPost];
+  const sql = `select count(1)  as like
+                from EstadoLike where idpost=$1
+                    `;
+  const result = await db.query(sql, params);
+
+  res.json(result);
+};
+
+const postLikeEvento = async (req, res) => {
+  try {
+    const { idPost, idUsuarios } = req.body;
+    const params = [idPost, idUsuarios];
+
+    const sql = `
+      INSERT INTO estadolike (idPost, idUsuarios)
+      VALUES ($1, $2)
+      RETURNING *;
+    `;
+    const result = await db.query(sql, params);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al insertar like post" });
+  }
+};
+
+const deleteLikeEvento = async (req, res) => {
+  try {
+    const { idPost, idUsuarios } = req.body;
+    const params = [idPost, idUsuarios];
+    const sql = `
+        DELETE FROM estadolike
+        WHERE idpost = $1 AND idUsuarios = $2
         RETURNING *;
       `;
     const result = await db.query(sql, params);
@@ -48,11 +99,11 @@ const deleteLike = async (req, res) => {
 // CRUD de Eventos
 const postSaveEvento = async (req, res) => {
   try {
-    const { idPost, idUsuarios } = req.body;
-    const params = [idPost, idUsuarios];
-console.log(params)
+    const { idEvento, idUsuarios } = req.body;
+    const params = [idEvento, idUsuarios];
+
     const sql = `
-      INSERT INTO save_evento (idPost, idUsuarios)
+      INSERT INTO save_evento (idEvento, idUsuarios)
       VALUES ($1, $2)
       RETURNING *;
     `;
@@ -67,15 +118,16 @@ console.log(params)
 
 const deleteSaveEvento = async (req, res) => {
   try {
-    const { idPost, idUsuarios } = req.body;
-    const params = [idPost, idUsuarios];
+    const { idEvento, idUsuarios } = req.body;
+    const params = [idEvento, idUsuarios];
     const sql = `
       DELETE FROM save_evento
-      WHERE idPost = $1 AND idUsuarios = $2
-      RETURNING *;
+      WHERE idevento = $1 AND idUsuarios = $2
+      RETURNING * ;
     `;
     const result = await db.query(sql, params);
     res.json(result);
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Error al eliminar evento" });
@@ -87,7 +139,7 @@ const getSaveEvento = async (req, res) => {
 
   const sql = `SELECT * , encode(foto, 'base64') AS imgevento
                 FROM SAVE_EVENTO SE
-                INNER JOIN EVENTO E ON  SE.IDpost=e.id
+                INNER JOIN EVENTO E ON  SE.idevento=e.id
                 where se.idUsuarios=$1
                     `;
   const result = await db.query(sql, params);
@@ -95,28 +147,47 @@ const getSaveEvento = async (req, res) => {
   res.json(result);
 };
 
+
+
 // crud de post
-const postInsertPost = async (req, res) => {
-  try {
-    const { encabezado, descripcion, idUsuarios } = req.body;
-
-    // Si estás usando multer, puedes acceder a los datos del archivo desde req.file
-    const buffer = req.file ? req.file.buffer : null;
-
-    const params = [encabezado, descripcion, buffer, idUsuarios];
-
-    const sql = `
-      INSERT INTO post (encabezado, descripcion, foto, idUsuarios) 
-      VALUES ($1, $2, $3, $4)
-      RETURNING *;
-    `;
-    const result = await db.query(sql, params);
-    res.json(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error al insertar post" });
-  }
+const getHiloPost = async (req, res) => {
+  const params = [req.params.idPost];
+ 
+  const sql = `select descripcion ,
+                      encode(foto, 'base64') AS foto ,
+                      encode(fotoPerfil, 'base64') AS fotousuario ,
+                      u.nombre, CASE
+                                  WHEN EXTRACT(YEAR
+                                                FROM AGE(NOW(), p.fecha_post)) > 0 THEN CONCAT('hace ', EXTRACT(YEAR
+                                                                                                                FROM AGE(NOW(), p.fecha_post)), ' año(s)')
+                                  WHEN EXTRACT(MONTH
+                                                FROM AGE(NOW(), p.fecha_post)) > 0 THEN CONCAT('hace ', EXTRACT(MONTH
+                                                                                                                FROM AGE(NOW(), p.fecha_post)), ' mes(es)')
+                                  WHEN EXTRACT(DAY
+                                                FROM AGE(NOW(), p.fecha_post)) > 0 THEN CONCAT('hace ', EXTRACT(DAY
+                                                                                                                FROM AGE(NOW(), p.fecha_post)), ' día(s)')
+                                  WHEN EXTRACT(HOUR
+                                                FROM AGE(NOW(), p.fecha_post)) > 0 THEN CONCAT('hace ', EXTRACT(HOUR
+                                                                                                                FROM AGE(NOW(), p.fecha_post)), ' hora(s)')
+                                  WHEN EXTRACT(MINUTE
+                                                FROM AGE(NOW(), p.fecha_post)) > 0 THEN CONCAT('hace ', EXTRACT(MINUTE
+                                                                                                                FROM AGE(NOW(), p.fecha_post)), ' minuto(s)')
+                                  ELSE 'Recién publicado'
+                              END AS tiempo_transcurrido
+                    FROM post p
+                    INNER JOIN usuarios u ON p.idusuarios = u.id
+                    where idpadre=
+                      (select id
+                        FROM post p
+                        where idpadre=0
+                            and id=$1)
+                    `;
+               
+  const result = await db.query(sql, params);
+console.log('hilos')
+  res.json(result);
 };
+
 
 const getPost = async (req, res) => {
   const params = [req.params.idUsuarios];
@@ -143,13 +214,58 @@ const getPost = async (req, res) => {
                       INNER JOIN 
                         usuarios u ON p.idusuarios = u.id
                       WHERE 
-                        idUsuarios = $1 
+                        idUsuarios = $1 and idpadre=0
                       ORDER BY 
                         p.id DESC;
                     `;
   const result = await db.query(sql, params);
 
   res.json(result);
+};
+
+const postInsertPost = async (req, res) => {
+  try {
+    const { encabezado, descripcion, idUsuarios } = req.body;
+
+
+    const buffer = req.file ? req.file.buffer : null;
+
+    const params = [encabezado, descripcion, buffer, idUsuarios];
+
+    const sql = `
+      INSERT INTO post (encabezado, descripcion, foto, idUsuarios) 
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `;
+    const result = await db.query(sql, params);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al insertar post" });
+  }
+};
+
+const postInsertHiloPost = async (req, res) => {
+  try {
+    console.log(1)
+    const { descripcion, idUsuarios, idPadre } = req.body;
+
+
+    const buffer = req.file ? req.file.buffer : null;
+
+    const params = [descripcion, idUsuarios, idPadre, buffer,];
+    console.log(params)
+    const sql = `
+      INSERT INTO post (descripcion, idUsuarios,idPadre, foto) 
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `;
+    const result = await db.query(sql, params);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al insertar post" });
+  }
 };
 
 
@@ -162,4 +278,9 @@ export {
   getSaveEvento,
   postInsertPost,
   getPost,
+  getLikeCountPost,
+  postLikeEvento,
+  deleteLikeEvento,
+  postInsertHiloPost,
+  getHiloPost,
 };
